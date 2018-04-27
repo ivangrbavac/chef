f300cd65897c2cffd44d3e8f0c48f9dae610f018
ivangrbavac <ivan.grbavac@energit.biz>
2018-04-27 16:19:36 +0200

    Update Chef ICO.sol

--- Chef ICO.sol
+++ Chef ICO.sol
@@ -1,189 +1,197 @@
-pragma solidity ^0.4.21;
+/*
+ kratki opis značajnijih funkcija:
+--reservedAmount funkcija prima iznos u ETH koji će se rezervirati za uplatu većih investitora u posebnom smart contractu. 
+    Navedeni iznos se odbija od hardCap-a definiranog u ovom ugovoru kako bi se smartcontract mogao automatski zaustaviti u slučaju dolaska do hardCap-a.
+--setSoftCapStatus je funkcija koja će se morati pozvati ako se uplata iz ugovora s Velikim investitorom nije ostvarila,
+    a zbog nje je definirano da je probijen softCap ili u obrnutom slučaju.
+--setFinalBonus funcija služi da bi se u zadnjem periodu ICO-a mogao povećati bonus s 0% na neku veću razinu ako se za takav korak odluči uprava
+--safeWithdrawal funkcija služi da bi osoba u slučaju neuspješnog ICO-a mogla povući ETH natrag na svoj račun. 
+--chefOwnerWithdrawal funkcija služi da bi vlasnik tokena u slučaju uspješnog ICO-a mogao povući ETH na svoj račun. Isplata tokena sudionicima ICO-a će se raditi ručno
+    nakon što se provjeri KYC za svaku osobu te nakon što se pokuša provjeriti da pojedina osoba nije s više adresa uplatila iznos veći od 10 000 eura. 
+    
+Funkcija za uplatu ETH ima sljedeće karakteristike:
+- nije moguće raditi uplate nakon što je skupljen hardCap
+- osoba ne može raditi uplate nakon što je uplatila više od 100 ETH
+- moguće je uplaćivati ETH samo prije 01.07.2018 i to iznose ne manje od 0.2 ETH
+- ako je osoba uplatila određeni iznos s kojim je pređen hardCap, iznos preko hardCap-a će biti vraćen osobi
+- ako je osoba uplatila ili zadnjom uplatom prešla preko 100ETH, inos preko 100 ETH će joj biti vraćen
+- ako je osoba napravila cjelovitu uplatu od 10ETH ili više, ostvaruje 15% bonusa koji se pribraja osnovnom bonusu
+- osnovni bonusi su :
+    *za uplatu u prvih 10 dana ICO-a osoba ostvaruje 20% bonusa
+    *za uplatu u narednih 10 dana ICO-a osoba ostvaruje 15% bonusa
+    *za uplatu u narednih 10 dana ICO-a osoba ostvaruje 10% bonusa
+    *za uplatu u narednih 10 dana ICO-a osoba ostvaruje 5% bonusa
+*/
 
-// ----------------------------------------------------------------------------
-// functions for safe conductiong of math operations
-// ----------------------------------------------------------------------------
-contract SafeOperations 
-{
-    function Add(uint a, uint b) public pure returns (uint c) 
-    {
-        c = a + b;
-        require(c >= a);
-    }
-    function Sub(uint a, uint b) public pure returns (uint c) 
-    {
-        require(b <= a);
-        c = a - b;
-    }
-    function Mul(uint a, uint b) public pure returns (uint c) 
-    {
-        c = a * b;
-        require(a == 0 || c / a == b);
-    }
-    function Div(uint a, uint b) public pure returns (uint c) 
-    {
-        require(b > 0);
-        c = a / b;
-    }
-}
-// ----------------------------------------------------------------------------
-// interface of CHEF smart contract function, used for transfering CHEF tokens 
-// to crowdsale contributors in a case of successful ICO
-// ----------------------------------------------------------------------------
-interface ChefToken {
-    function transferFromCroudsale(address receiver, uint value) external;
-}
+  pragma solidity 0.4.23;
+  import "github.com/OpenZeppelin/zeppelin-solidity/contracts/math/SafeMath.sol";
 
+contract ChefICO {
+    
+    using SafeMath for uint256;
+    
+    uint256 public softCap;
+	uint256 public hardCap;
+	uint256 public tempHardCap;
+    uint256 public totalAmount;
+    uint256 public reservedAmount;
+    uint256 public chefPrice;
+    uint256 public minimumInvestment;
+    uint256 public maximumInvestment;
+    uint256 public finalBonus;
+    
+    uint256 public icoStart;
+    uint256 public icoEnd;
+    address public chefOwner;
 
+	bool public softCapReached = false;
+	bool public hardCapReached = false;
 
-contract ChefICO is SafeOperations {
-	ChefToken public Chef;
-    uint public softCap;
-	uint public hardCap;
-	address public recipient;
-    uint public totalAmount;
-    uint public ICOdeadline;
-    uint public hardCapReachedDeadline;
-    uint public ChefPrice;
-    uint public bonusDeadline; //deadline for additional 5% in ICO
-    uint minimumInvestment;
-    uint maximumInvestment;
-	bool softCapReached = false;
-    bool crowdsaleClosed = false;
-	bool hardCapReached = false;
-	address ChefAddress = 0x000000000000000000000000000; // adress needs to be set
-	address ChefOwner  =  0x000000000000000000000000000; // adress needs to be set
-	
 	mapping(address => uint256) public balanceOf;
-	mapping(address => uint256) public ChefBalanceOf;
+	mapping(address => uint256) public chefBalanceOf;
 
-    event ChefICOSucceed(address indexed recipient, uint totaltotalAmount);
+    event ChefICOSucceed(address indexed recipient, uint totalAmount);
     event ChefICOTransfer(address indexed tokenHolder, uint value, bool isContribution);
 
- //------------------------------------------
- // initialize ICO parameters
- //------------------------------------------
+
     function ChefICO() public {
-        recipient = ChefAddress;
         softCap = 7000 * 1 ether;
         hardCap = 22500 * 1 ether;
-        ICOdeadline = 1530406800; //01.07.2018. 00:00:00 CET
-        hardCapReachedDeadline=1530406800;
-        bonusDeadline = 1527728400; //02.06.2018 00:00:00 CET 
-        ChefPrice = 0.0001 * 1 ether;
-        minimumInvestment=0.2 * 1 ether;
+        tempHardCap = 22500 * 1 ether;
+        totalAmount = 0;
+        reservedAmount = 0;
+        chefPrice = 0.0001 * 1 ether;
+        minimumInvestment = 1 ether / 5;
         maximumInvestment = 100 * 1 ether;
-        Chef = ChefToken(ChefAddress);
+        finalBonus = 100;
+
+        icoStart = 1525471200;
+        icoEnd = 1530396000;
+        chefOwner = msg.sender;
+    }
+    
+    
+    function balanceOf(address _contributor) public view returns (uint256 balance) {
+        return balanceOf[_contributor];
+    }
+    
+    
+    function chefBalanceOf(address _contributor) public view returns (uint256 balance) {
+        return chefBalanceOf[_contributor];
     }
 
 
-//------------------------------------------
-//function thet is trigerred on every payment. It stores data about paid amount
-// and stops ICO if hard cap is reached.
-//------------------------------------------
-    function () public payable {
-        require(!crowdsaleClosed && !hardCapReached);
-        uint amount = msg.value;
-        require(amount>=minimumInvestment && balanceOf[msg.sender]<maximumInvestment);
+    modifier onlyOwner() {
+        require(msg.sender == chefOwner);
+        _;
+    }
+    
+    
+    modifier afterICOdeadline() { 
+        if (now >= icoEnd )
+            _; 
+        }
         
-        if(hardCap >= Add(totalAmount,amount))
-        {
-            hardCapReached=true;
-            crowdsaleClosed = true;
-            emit ChefICOSucceed(recipient, hardCap);
+    modifier beforeICOdeadline() { 
+        if (now <= icoEnd )
+            _; 
+        }
+    
+   
+    function setFinalBonus(uint256 _value) public onlyOwner returns (bool success) {
+        finalBonus = _value;
+        return true;    
+    }
+
+
+    function () public payable beforeICOdeadline {
+        uint256 amount = msg.value;
+        require(!hardCapReached);
+        require(amount >= minimumInvestment && balanceOf[msg.sender] < maximumInvestment);
+        
+        if(tempHardCap <= totalAmount.add(amount)) {
+            hardCapReached = true;
+            emit ChefICOSucceed(chefOwner, hardCap);
             
-             if(hardCap > Add(totalAmount,amount)){
-                uint returnAmount=Sub(Add(totalAmount,amount),hardCap);
+             if(tempHardCap < totalAmount.add(amount)) {
+                uint256 returnAmount = totalAmount.add(amount).sub(tempHardCap);
                 msg.sender.transfer(returnAmount);
                 emit ChefICOTransfer(msg.sender, returnAmount, false);
-                amount= Sub(amount,returnAmount);    
+                amount = amount.sub(returnAmount);    
              }
         }
         
-        if(maximumInvestment>Add(balanceOf[msg.sender],amount))
-        {
-          uint overMaxAmount=Sub(Add(balanceOf[msg.sender],amount),maximumInvestment);
+        if(maximumInvestment < balanceOf[msg.sender].add(amount)) {
+          uint overMaxAmount = balanceOf[msg.sender].add(amount).sub(maximumInvestment);
           msg.sender.transfer(overMaxAmount);
           emit ChefICOTransfer(msg.sender, overMaxAmount, false);
-          amount= Sub(amount,overMaxAmount);
+          amount = amount.sub(overMaxAmount);
         }
+
+        totalAmount = totalAmount.add(amount);
+        balanceOf[msg.sender] = balanceOf[msg.sender].add(amount);
         
-        
-        balanceOf[msg.sender] =Add(balanceOf[msg.sender] ,amount);
-        totalAmount =Add(totalAmount, amount);
-        
-        //give 5% bonuse if payment was made before bonusDeadline
-        if (now <= bonusDeadline) {
-            ChefBalanceOf[msg.sender]=Div(Mul(amount, 1.05 ether), ChefPrice);
+
+        uint256 additionalBonus = 0;
+        if (amount >= 10 ether) {
+            additionalBonus=15;
         }
-        else
-        {
-            ChefBalanceOf[msg.sender]=Div(amount, ChefPrice);
+                
+        if (now <= icoStart.add(10 days)) {
+            chefBalanceOf[msg.sender] = amount.div(chefPrice).mul(120 + additionalBonus).div(100);
+        }
+        else if (now <= icoStart.add(20 days)) {
+            chefBalanceOf[msg.sender] = amount.div(chefPrice).mul(115 + additionalBonus).div(100);
+        }
+        else if (now <= icoStart.add(30 days)) {
+            chefBalanceOf[msg.sender] = amount.div(chefPrice).mul(110 + additionalBonus).div(100);
+        }
+        else if (now <= icoStart.add(40 days)) {
+            chefBalanceOf[msg.sender] = amount.div(chefPrice).mul(105 + additionalBonus).div(100);
+        }
+        else {
+            chefBalanceOf[msg.sender] = amount.div(chefPrice).mul(finalBonus + additionalBonus).div(100);
         }
         
         emit ChefICOTransfer(msg.sender, amount, true);
         
-        if (totalAmount >= softCap && softCapReached==false ){
-        softCapReached= true;
-        emit ChefICOSucceed(recipient, totalAmount);
+        if (totalAmount >= softCap && softCapReached == false ){
+        softCapReached = true;
+        emit ChefICOSucceed(chefOwner, totalAmount);
         }
     }
 
-//------------------------------------------
-//modifier that checks ICO deadline
-//------------------------------------------
-    modifier afterICOdeadline() { if (now >= ICOdeadline || now >= hardCapReachedDeadline) _; }
-
-
-//------------------------------------------
-//closes crowdsale, on a defined deadline,
-// if soft cap is reached
-//------------------------------------------
-    function checkGoalReached() public afterICOdeadline {
-        if (totalAmount >= softCap){
-           crowdsaleClosed = true;
-        }
-        
-    }
-
-//------------------------------------------
-//if hadr cap is reached, this function sets the date on which
-//contributors can withdrow their chef tokens. It can only be executed by 
-//ChefOwner address 
-//------------------------------------------
-function setHardCapReachedDeadline (uint withdrawalDate) public 
-{
-    if (msg.sender == ChefOwner){
-    hardCapReachedDeadline=withdrawalDate;
-    }
-}
-
-//------------------------------------------
-// function for fund withdrawals. if soft cap is not reached, 
-// function returns ETH amount to contributors.
-// If soft/hard cap is reached, and time limit passed,
-// function sends Chef tokens to contributor and ETH to ChefOwner address
-//------------------------------------------
-  
-    function safeWithdrawal() public afterICOdeadline  {
-        uint amount = balanceOf[msg.sender];
-        balanceOf[msg.sender] = 0;
-        if (amount > 0) {
-            if (!softCapReached) {
-                    msg.sender.transfer(amount);
-                    emit ChefICOTransfer(msg.sender, amount, false);
-            }
-            
-            if (softCapReached && recipient != msg.sender) {
-                Chef.transferFromCroudsale(msg.sender, ChefBalanceOf[msg.sender]);
-                emit  ChefICOTransfer(msg.sender, ChefBalanceOf[msg.sender], true);
-            }
     
-            if (softCapReached && recipient == msg.sender) {
-                recipient.transfer(totalAmount);
-                emit  ChefICOTransfer(recipient, totalAmount, false);
-                
+    function reservedAmount(uint256 _value) public onlyOwner {
+		_value = _value * 1 ether;
+        require(totalAmount.add(_value) <= hardCap);
+        reservedAmount = _value;
+        tempHardCap = hardCap.sub(_value);
+    }
+    
+    
+   function setSoftCapStatus (bool _value) public onlyOwner beforeICOdeadline {
+        require(hardCap > totalAmount.add(reservedAmount));
+		softCapReached = _value;
+	}
+
+
+   function safeWithdrawal() public afterICOdeadline {
+        if (!softCapReached) {
+			uint256 amount = balanceOf[msg.sender];
+            balanceOf[msg.sender] = 0;
+            if (amount > 0) {
+                msg.sender.transfer(amount);
+                emit ChefICOTransfer(msg.sender, amount, false);
             }
+        }
+    }
+        
+    
+    function chefOwnerWithdrawal() public afterICOdeadline onlyOwner {    
+        if (softCapReached) {
+            chefOwner.transfer(totalAmount);
+            emit ChefICOTransfer(chefOwner, totalAmount, false);
         }
     }
 }
